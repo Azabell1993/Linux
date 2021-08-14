851e27f956f95e2ebc3ba94488a95ea06d34ce29
Azabell1993 <Azabell1993>
2021-08-15 05:16:24 +0900

    commit

--- Printf_built_in_function/printf.c/include/az_puthex.c
+++ Printf_built_in_function/printf.c/include/az_puthex.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 
-static void	az_puthex_rec(unsigned int n)
+void	az_puthex(unsigned int n)
 {
 	char	c;
 	char	*map;
@@ -10,12 +10,8 @@ static void	az_puthex_rec(unsigned int n)
 	{
 		c = (char)(n & 0xF);
 		n = (n >> 4);
-		az_puthex_rec(n);
+		az_puthex(n);
 		az_putchar(map[(int)c]);
 	}
 }
 
-void		az_puthex(unsigned int n)
-{
-	az_puthex_rec(n);
-}
